ecccf142e16e10cc5f25c8ad2e0daa5783459ab9
BerniceChua <Bernice.Chua.415@gmail.com>
2021-06-12 19:34:22 -0700

    Organize test scenes into their own directory.

---
+++ Assets/Heisenberg/Scripts/RealitySwitcher.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealitySwitcher : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Box>(out var comp))
+        {
+            other.gameObject.layer.;
+            SwitchTo();
+        }
+    }
+
+    public void SwitchTo()
+    {
+
+    }
+}
