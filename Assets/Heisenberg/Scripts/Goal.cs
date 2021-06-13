7d2199097dd780bcd563b3142af70a350f1a1d7b
davidkol <davidkolop@gmail.com>
2021-06-13 01:17:04 -0700

    Goal animation and level switch timing

--- Assets/Heisenberg/Scripts/Goal.cs
+++ Assets/Heisenberg/Scripts/Goal.cs
@@ -6,10 +6,14 @@ using UnityEngine.SceneManagement;
 public class Goal : MonoBehaviour
 {
     public string SceneToLoad;
+    private Animator animator;
+    private CatController kitty;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponentInChildren<Animator>();
+        kitty = FindObjectOfType<CatController>();
     }
 
     // Update is called once per frame
@@ -18,7 +22,22 @@ public class Goal : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if( other.tag != "Reality1" )
+            return;
+        animator.SetBool("ReachGoal", true);
+        kitty.transform.position = transform.position;
+        kitty.enabled = false;
+    }
+
+    public void DisappearCat()
+    {
+        kitty.GetComponent<SpriteRenderer>().color = Color.clear;
+    }
+
+    public void LoadNextLevel()
+    {
         SceneManager.LoadScene(SceneToLoad);
     }
 }
