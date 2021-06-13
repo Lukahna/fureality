9cb2cb926b0d277976376b0463381e680530f768
Lukahna <sub@ry.ca>
2021-06-13 10:56:19 -0600

    Create Goal_Final.cs

---
+++ Assets/Heisenberg/Scripts/Goal_Final.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    public string SceneToLoad;
+    [SerializeField] private float _sceneDelay = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // Load the next level after a delay
+    public void LoadNextLevel()
+    {
+        StartCoroutine(NextLevelCo());
+    }
+
+    IEnumerator NextLevelCo() {
+        yield return new WaitForSeconds(sceneDelay);
+        SceneManager.LoadScene(SceneToLoad);
+    }
+}
